ad108378b0a79586fc03b35af15487cd23e0d9cc
akshatgg <akshatg9636@gmail.com>
2023-08-23 17:25:35 +0545

    modify

---
+++ Buttonjs/script.js
@@ -0,0 +1,23 @@
+
+let btn=document.getElementById("button");
+console.log(btn.innerText);
+btn.className="boot";
+let bt=document.getElementsByClassName("boot");
+console.log(bt.innerText);
+
+
+function randomcolor(){
+let val="0123456789ABCDEF";
+let cont="#";
+for(let i=0;i<6;i++){
+cont=cont+val[Math.floor(Math.random()*16)];
+}
+return cont;
+}
+
+
+function changecolor(){
+    document.body.style.backgroundColor=randomcolor();
+    
+}
+btn.addEventListener("click" ,changecolor); 
